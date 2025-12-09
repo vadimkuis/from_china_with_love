96b7fa8c42c90a4b039590ba46d6db7c0399c84a
Vadim Kuis <57957817+vadimkuis@users.noreply.github.com>
2025-12-09 11:01:08 +0300

    Delete js directory

--- js/data.js
+++
@@ -1,9 +0,0 @@
-let users = JSON.parse(localStorage.getItem('users')) || [];
-let orders = JSON.parse(localStorage.getItem('orders')) || [];
-let settings = JSON.parse(localStorage.getItem('settings')) || {
-    yuanRate: 13.5, kgPrice: 500, costYuan: 10, costKg: 300, taxPercent: 10,
-    deliveryMethods: ['Самовывоз', 'Европочта', 'Белпочта'],
-    forbiddenItems: ['Оружие', 'Наркотики']
-};
-let currentUser = null;
-const statusFlow = ['Ожидает оплаты','Оплачен','Выкуплен','Отправлен из Китая','Прибыл в Москву','Доставляется в Беларусь','Доставлен'];
